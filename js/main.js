95d3e59d7a4b08542c82cd96b4b50d212a1fa71c
Łukasz Lipski <ukasz Lipski>
2016-09-26 00:10:59 +0200

    Added events and basic drawing

--- js/main.js
+++ js/main.js
@@ -1,7 +1,38 @@
+var canvas,
+    context,
+    paint = false;
+
 window.onload = function() {
 
-    var canvas = document.getElementById('canvas');
-        context = canvas.getContext('2d');
+    canvas = document.getElementById('canvas');
+    context = canvas.getContext('2d');
+    let    width = canvas.width = 800,
+        height = canvas.height = 600;
+
+    canvas.addEventListener('mousedown', StartPaint, false);
+    canvas.addEventListener('mousemove', MovePaint, false);
+    canvas.addEventListener('mouseup', StopPaint, false);
+
 
     
+}
+
+function StartPaint() {
+    paint = true;
+    Draw(event.pageX-this.offsetLeft, event.pageY-this.offsetTop);
+}
+
+function MovePaint() {
+    if(paint)
+        Draw(event.pageX-this.offsetLeft, event.pageY-this.offsetTop);
+}
+
+function StopPaint() {
+    paint = false;
+}
+
+function Draw(width,height) {
+    context.beginPath();
+    context.arc(width,height, 5, 0, 2 * Math.PI, false);
+    context.fill();
 }
